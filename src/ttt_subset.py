e51e92efc897f131c464d9b9e575779a85cbef35
dijor0310 <dijor0310@gmail.com>
2025-08-17 16:13:09 +0200

    add spinach to subsets TTT

--- src/ttt_subset.py
+++ src/ttt_subset.py
@@ -69,7 +69,8 @@ def ttt(cfg):
     # val_loader = DataLoader([0])
 
     trainer = pl.Trainer(
-        max_epochs=cfg.method.max_epochs,
+        # max_epochs=cfg.method.max_epochs,
+        max_steps=cfg.method.max_steps,
         logger=(
             None
             if cfg.debug
